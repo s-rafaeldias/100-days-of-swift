ddde1e1492771a9c8848fa59c471c36ae1e2f366
Rafael Dias <rafa197795@gmail.com>
2020-07-03 22:35:06 -0300

    feat(day: 56): finish

--- 52-59/Project14/Project14/GameScene.swift
+++ 52-59/Project14/Project14/GameScene.swift
@@ -18,6 +18,7 @@ class GameScene: SKScene {
         }
     }
     
+    var numRounds = 0
     var popupTime = 0.85
     
     func createSlot(at position: CGPoint) {
@@ -28,6 +29,18 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
+        numRounds += 1
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            return
+        }
         popupTime *= 0.991
         
         slots.shuffle()
@@ -74,6 +87,26 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+
+            if node.name == "charFriend" {
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                score += 1
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
 }
