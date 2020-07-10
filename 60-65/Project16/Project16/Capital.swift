03851a2f619b4c1fb262758c634b238e6d006d72
Rafael Dias <rafa197795@gmail.com>
2020-07-10 20:45:40 -0300

    feat(day: 62 and 63): finish

---
+++ 60-65/Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Rafael Dias on 08/07/20.
+//  Copyright © 2020 Rafael Dias. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
